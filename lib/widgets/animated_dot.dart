431fe848da0720fbc2f7d83af29b0bc6ae61aab3
Son Chau <soncqb@gmail.com>
2019-06-04 16:14:32 -0500

    add animated dot

--- lib/widgets/animated_dot.dart
+++ lib/widgets/animated_dot.dart
@@ -29,3 +29,4 @@ class AnimatedDot extends AnimatedWidget {
           )),
     );
   }
+}
--- lib/widgets/price_tab.dart
+++ lib/widgets/price_tab.dart
@@ -1,4 +1,5 @@
 
+import 'package:flight_search/widgets/animated_dot.dart';
 import 'package:flight_search/widgets/animated_plan_icon.dart';
 import 'package:flutter/material.dart';
 
@@ -14,20 +15,88 @@ class PriceTab extends StatefulWidget {
 class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
   final double _initialPlanePaddingBottom = 16.0;
   final double _minPlanePaddingTop = 16.0;
+  final List<int> _flightStops = [1, 2, 3, 4];
+  final double _cardHeight = 80.0;
 
   AnimationController _planeSizeAnimationController;
   AnimationController _planeTravelController;
+  AnimationController _dotsAnimationController;
   Animation _planeSizeAnimation;
   Animation _planeTravelAnimation;
+
+  List<Animation<double>> _dotPositions = [];
 
   double get _planeTopPadding =>
       _minPlanePaddingTop +
           (1 - _planeTravelAnimation.value) * _maxPlaneTopPadding;
 
   double get _maxPlaneTopPadding =>
-      widget.height - _initialPlanePaddingBottom - _planeSize;
+      widget.height -
+          _minPlanePaddingTop -
+          _initialPlanePaddingBottom -
+          _planeSize;
 
   double get _planeSize => _planeSizeAnimation.value;
+
+  @override
+  void initState() {
+    super.initState();
+    _initSizeAnimations();
+    _initPlaneTravelAnimations();
+    _initDotAnimationController();
+    _initDotAnimations();
+    _planeSizeAnimationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _planeSizeAnimationController.dispose();
+    _planeTravelController.dispose();
+    _dotsAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[_buildPlane()]
+          ..addAll(_flightStops.map(_mapFlightStopToDot)),
+      ),
+    );
+  }
+
+  Widget _mapFlightStopToDot(stop) {
+    int index = _flightStops.indexOf(stop);
+    bool isStartOrEnd = index == 0 || index == _flightStops.length - 1;
+    Color color = isStartOrEnd ? Colors.red : Colors.green;
+    return AnimatedDot(
+      animation: _dotPositions[index],
+      color: color,
+    );
+  }
+
+  Widget _buildPlane() {
+    return AnimatedBuilder(
+      animation: _planeTravelAnimation,
+      child: Column(
+        children: <Widget>[
+          AnimatedPlaneIcon(animation: _planeSizeAnimation),
+          Container(
+            width: 2.0,
+            height: _flightStops.length * _cardHeight * 0.8,
+            color: Color.fromARGB(255, 200, 200, 200),
+          ),
+        ],
+      ),
+      builder: (context, child) => Positioned(
+        top: _planeTopPadding,
+        child: child,
+      ),
+    );
+  }
 
   _initSizeAnimations() {
     _planeSizeAnimationController = AnimationController(
@@ -35,14 +104,20 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
       vsync: this,
     )..addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        Future.delayed(Duration(milliseconds: 500),
-                () => _planeTravelController.forward());
+        Future.delayed(Duration(milliseconds: 500), () {
+//          widget?.onPlaneFlightStart();
+          _planeTravelController.forward();
+        });
+        Future.delayed(Duration(milliseconds: 700), () {
+          _dotsAnimationController.forward();
+        });
       }
     });
-    _planeSizeAnimation = Tween<double>(begin: 60.0, end: 36.0).animate(CurvedAnimation(
-      parent: _planeSizeAnimationController,
-      curve: Curves.easeOut,
-    ));
+    _planeSizeAnimation =
+        Tween<double>(begin: 60.0, end: 36.0).animate(CurvedAnimation(
+          parent: _planeSizeAnimationController,
+          curve: Curves.easeOut,
+        ));
   }
 
   _initPlaneTravelAnimations() {
@@ -56,49 +131,37 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _initSizeAnimations();
-    _initPlaneTravelAnimations();
-    _planeSizeAnimationController.forward();
+  void _initDotAnimations() {
+    //what part of whole animation takes one dot travel
+    final double slideDurationInterval = 0.4;
+    //what are delays between dot animations
+    final double slideDelayInterval = 0.2;
+    //at the bottom of the screen
+    double startingMarginTop = widget.height;
+    //minimal margin from the top (where first dot will be placed)
+    double minMarginTop =
+        _minPlanePaddingTop + _planeSize + 0.5 * (0.8 * _cardHeight);
+
+    for (int i = 0; i < _flightStops.length; i++) {
+      final start = slideDelayInterval * i;
+      final end = start + slideDurationInterval;
+
+      double finalMarginTop = minMarginTop + i * (0.8 * _cardHeight);
+      Animation<double> animation =  Tween(
+        begin: startingMarginTop,
+        end: finalMarginTop,
+      ).animate(
+         CurvedAnimation(
+          parent: _dotsAnimationController,
+          curve:  Interval(start, end, curve: Curves.easeOut),
+        ),
+      );
+      _dotPositions.add(animation);
+    }
   }
 
-  @override
-  void dispose() {
-    _planeSizeAnimationController.dispose();
-    super.dispose();
+  void _initDotAnimationController() {
+    _dotsAnimationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 500));
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[_buildPlane()],
-      ),
-    );
-  }
-
-  Widget _buildPlane() {
-    return AnimatedBuilder(
-      animation: _planeTravelAnimation,
-      child: Column(
-        children: <Widget>[
-          AnimatedPlaneIcon(animation: _planeSizeAnimation),
-          Container(
-            width: 2.0,
-            height: 240.0,
-            color: Color.fromARGB(255, 200, 200, 200),
-          ),
-        ],
-      ),
-      builder: (context, child) => Positioned(
-        top: _planeTopPadding,
-        child: child,
-      ),
-    );
-  }
-
 }
