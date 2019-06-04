28907a5a0415f6dd0285f1959a6acc7a91c20174
Son Chau <soncqb@gmail.com>
2019-06-04 15:15:42 -0500

    fly animation

--- lib/widgets/price_tab.dart
+++ lib/widgets/price_tab.dart
@@ -1,4 +1,5 @@
 
+import 'package:flight_search/widgets/animated_plan_icon.dart';
 import 'package:flutter/material.dart';
 
 class PriceTab extends StatefulWidget {
@@ -11,10 +12,20 @@ class PriceTab extends StatefulWidget {
 }
 
 class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
-  AnimationController _planeSizeAnimationController;
-  Animation _planeSizeAnimation;
+  final double _initialPlanePaddingBottom = 16.0;
+  final double _minPlanePaddingTop = 16.0;
 
-  double get _planeTopPadding => widget.height - _initialPlanePaddingBottom - _planeSize;
+  AnimationController _planeSizeAnimationController;
+  AnimationController _planeTravelController;
+  Animation _planeSizeAnimation;
+  Animation _planeTravelAnimation;
+
+  double get _planeTopPadding =>
+      _minPlanePaddingTop +
+          (1 - _planeTravelAnimation.value) * _maxPlaneTopPadding;
+
+  double get _maxPlaneTopPadding =>
+      widget.height - _initialPlanePaddingBottom - _planeSize;
 
   double get _planeSize => _planeSizeAnimation.value;
 
@@ -22,17 +33,34 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
     _planeSizeAnimationController = AnimationController(
       duration: const Duration(milliseconds: 340),
       vsync: this,
-    );
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Future.delayed(Duration(milliseconds: 500),
+                () => _planeTravelController.forward());
+      }
+    });
     _planeSizeAnimation = Tween<double>(begin: 60.0, end: 36.0).animate(CurvedAnimation(
       parent: _planeSizeAnimationController,
       curve: Curves.easeOut,
     ));
   }
 
+  _initPlaneTravelAnimations() {
+    _planeTravelController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _planeTravelAnimation = CurvedAnimation(
+      parent: _planeTravelController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
     _initSizeAnimations();
+    _initPlaneTravelAnimations();
     _planeSizeAnimationController.forward();
   }
 
@@ -54,21 +82,18 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
   }
 
   Widget _buildPlane() {
-    return Positioned(
-      top: _planeTopPadding,
+    return AnimatedBuilder(
+      animation: _planeTravelAnimation,
       child: Column(
         children: <Widget>[
-          _buildPlaneIcon(),
+          AnimatedPlaneIcon(animation: _planeSizeAnimation),
         ],
+      ),
+      builder: (context, child) => Positioned(
+        top: _planeTopPadding,
+        child: child,
       ),
     );
   }
 
-  Widget _buildPlaneIcon() {
-    return Icon(
-      Icons.airplanemode_active,
-      color: Colors.red,
-      size: _planeSize,
-    );
-  }
 }
